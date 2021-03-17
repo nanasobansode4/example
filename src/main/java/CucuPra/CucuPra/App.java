203e879ab519bbf03f79144186be95578435092b
Nana <nanasobansode4@gmail.com>
2021-03-17 23:27:57 +0530

    all fil

---
+++ src/main/java/CucuPra/CucuPra/App.java
@@ -0,0 +1,13 @@
+package CucuPra.CucuPra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
